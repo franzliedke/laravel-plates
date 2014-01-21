ab5928e26919a6b01be7870046151f022711bea2
Franz Liedke <franz@develophp.org>
2014-01-22 00:03:09 +0100

    Clean up service provider, extend view in register().

--- src/Franzl/LaravelPlates/LaravelPlatesServiceProvider.php
+++ src/Franzl/LaravelPlates/LaravelPlatesServiceProvider.php
@@ -5,42 +5,23 @@ use Illuminate\Support\ServiceProvider;
 class LaravelPlatesServiceProvider extends ServiceProvider {
 
 	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-
-	/**
 	 * Register the service provider.
 	 *
 	 * @return void
 	 */
 	public function register()
 	{
-		//
-	}
-
-	public function boot()
-	{
 		$app = $this->app;
-
-		$app['view']->addExtension('plates.php', 'plates', function() use ($app)
+		
+		$app->resolving('view', function($view) use ($app)
 		{
-			$path = $app['config']['view.paths'][0];
-
-			return new Engine($path, 'plates.php');
+			$view->addExtension('plates.php', 'plates', function() use ($app)
+			{
+				$path = $app['config']['view.paths'][0];
+				
+				return new Engine($path, 'plates.php');
+			})
 		});
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
 	}
 
 }
